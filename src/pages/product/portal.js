df633bc10a7484a2f4125ecc5a0a94fdc19b7119
Garth <244253+xgp@users.noreply.github.com>
2023-01-15 09:43:17 +0100

    stub pages in src/product dir

---
+++ src/pages/product/portal.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import classnames from 'classnames';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './../styles.module.css';
+import { render } from "react-dom";
+
+var ReactRotatingText = require('react-rotating-text');
+
+function getStarted() {
+  window.open(
+    `https://phasetwo.io/dashboard/`,
+    '_blank'
+  );
+}
+   
+function Portal() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  return (
+    <Layout
+      title={`${siteConfig.title}`}
+      description="Tools for SaaS builders">
+
+      {/* Main Content */}
+      <main>
+
+        {/* Hero Section */}
+        <div>main content here</div>
+
+      </main>
+
+    </Layout>
+  );
+}
+
+export default Portal;
